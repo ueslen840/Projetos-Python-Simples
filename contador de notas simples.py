9573d320b3fa9aa8d8c4c35fc2b2810ba0dcbcbd
ueslen840 <81395023+ueslen840@users.noreply.github.com>
2021-10-31 16:16:52 -0400

    Update contador de notas simples.py

--- contador de notas simples.py
+++ contador de notas simples.py
@@ -36,13 +36,29 @@ while fim<1: # aqui o software vai fazer a contagem ate acabar
         valorrestante=valorrestante-2
     os.system("cls") #limpar terminal
     fim=1
-print (" as notas a sacar são.")
-print (" notas de 200: ",nota200)
-print (" notas de 100: ",nota100)
-print (" notas de 50: ",nota50)
-print (" notas de 20: ",nota20)
-print (" notas de 10: ",nota10)
-print (" notas de 5: ",nota5)
-print (" notas de 2: ",nota2)
 
-print (" resto nao possivel de sacar: "); print(valorrestante) #nota de 1 real nao esta mais em circulaçao no brasil.
+print (" as notas a sacar são.")
+# os "IF's" aqui, sao para aparecer no console apenas as notas que sairiam do caixa, omitindo as com valor 0.
+if nota200>0:
+    print (" notas de 200: ",nota200)
+
+if nota100>0:
+    print (" notas de 100: ",nota100)
+
+if nota50>0:
+    print (" notas de 50: ",nota50)
+
+if nota20>0:
+    print (" notas de 20: ",nota20)
+
+if nota100>0:
+    print (" notas de 10: ",nota10)
+
+if nota5>0:
+    print (" notas de 5: ",nota5)
+
+if nota2>0:
+    print (" notas de 2: ",nota2)
+
+if valorrestante>0:
+    print (" resto nao possivel de sacar: ",valorrestante) #nota de 1 real nao esta mais em circulaçao no brasil.
